9d6678c6a440b161e541ddf6073eb72be3fa7305
PranavMKSB <pranavmksb@gmail.com>
2022-10-21 22:43:09 -0700

    DistanceAuto encoders if else statement.

---
+++ src/main/java/frc/robot/commands/TimedAuto.java
@@ -0,0 +1,55 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.Timer;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.DriveTrain;
+
+
+public class TimedAuto extends CommandBase {
+  private final Timer timer = new timer();
+  private final DriveTrain drive = new DriveTrain();
+  public TimedAuto() {}
+
+
+  /** Creates a new TimedAuto. */
+  public TimedAuto() {
+    timer.reset();
+    addRequirements(drive);
+    // Use addRequirements() here to declare subsystem dependencies.
+  }
+
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    timer.start();
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    if(timer.get() < 2){
+      drive.tankdrive(-0.3,-0.3);
+    }
+    else if(timer.get() < 3){
+      drive.tankDrive(-0.3,-0.3);
+    }
+   }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    if(timer.get() > 4){
+      return true;
+    }
+    return false;
+  }
+}
